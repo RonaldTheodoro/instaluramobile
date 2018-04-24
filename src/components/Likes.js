bb64890ef1b525d130b97ac01fc1948f4b703e7c
RonaldTheodoro <ronald.silva4@fatec.sp.gov.br>
2018-04-23 22:13:28 -0300

    create  Likes component

---
+++ src/components/Likes.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TouchableOpacity
+} from 'react-native';
+
+
+export default class Likes extends Component {
+
+  carregaIcone(likeada) {
+    if (likeada)
+      return require('../../resources/img/s2-checked.png')
+    return require('../../resources/img/s2.png')
+  }
+
+  exibeLikes(likers) {
+    if (likers.length === 0)
+      return
+
+    return (
+      <Text style={styles.likes}>
+        {likers.length} {likers.length > 1 ? 'curtidas' : 'curtida'}
+      </Text>
+    )
+  }
+
+  render() {
+    const { foto, likeCallBack } = this.props
+    return (
+      <View>
+        <TouchableOpacity onPress={likeCallBack}>
+          <Image
+            style={styles.botaoDeLike}
+            source={this.carregaIcone(foto.likeada)} />
+        </TouchableOpacity>
+        {this.exibeLikes(foto.likers)}
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  botaoDeLike: {
+    height: 40,
+    width: 40
+  },
+  likes: {
+    fontWeight: 'bold',
+    marginBottom: 10
+  },
+});
